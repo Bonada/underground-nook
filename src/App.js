cb93b38da23ee67cc36b206397441a171e46573d
Wyvail <47842470+Wyvail@users.noreply.github.com>
2021-10-24 12:05:29 -0400

    Routing prototype

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import micans from './Micans.png';
 import nanaouk from './Nanaouk.png';
 import cebublue from './CebuBlue.png';
 import minying from './Minying.jpg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 
 function CatalogCard() {
@@ -25,35 +31,83 @@ function CatalogCard() {
     </div>);
 }
 
-function App() {
+function Catalog() {
   return (
-    <header className="App-header container-fluid">
-      <div>
-        <img src={logo} className="App-logo" alt="logo" />
-        Navbar goes here
-      </div>
+    <div>
       <div className="row">
         <div className="offset-md-1 col-md-3">
           <h1>Catalog</h1>
           <p>a collection of our plants</p>
         </div>
       </div>
-      <div className="container-fluid">
+      <div className="container-fluid"></div>
+      <div className="row cards">
+        <div className="row">&nbsp;</div>
+        <div className="col-md-1"></div>
+        <CatalogCard />
+        <CatalogCard />
+        <CatalogCard />
+        <CatalogCard />
+        <CatalogCard />
+      </div>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <div className="row">
+        <div className="offset-md-1 col-md-8">
+          <h1>About Us</h1>
+          <p>Underground Nook is an NYC-based company dedicated to selling plants.</p>
+        </div>
+      </div><div className="container-fluid">
         <div className="row cards">
           <div className="row">&nbsp;</div>
-          <div className="col-md-1"></div>
-          <CatalogCard />
-          <CatalogCard />
-          <CatalogCard />
-          <CatalogCard />
-          <CatalogCard />
-          
+          <div className="offset-md-1 col-md-4 d-flex justify-content-center">
+            <img src={minying} className="profile" alt="Minying Cao"></img>
+          </div>
+          <div className="col-md-5">
+            Minying Cao is the proud owner of Underground Nook. She started this company as a pastime during the
+            pandemic and continues to operate out of her home in New York City, where she takes care of plants and sells
+            them online.
+          </div>
         </div>
       </div>
-    </header>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <header className="App-header container-fluid">
+        <div>
+          <ul>
+            <li>
+              <Link to="/Catalog">Catalog</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+          </ul>
+        </div>
+        <Switch>
+          <Route path="/Catalog">
+            <Catalog />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </header>
+    </Router >
   );
 }
 
 export default App;
+
+
 
 
