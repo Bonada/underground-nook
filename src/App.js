2010b6e672ca1b29248f69b9bac73e5ba05ec8cf
sahith00 <sahithb00@gmail.com>
2021-10-27 15:40:26 -0400

    Fixed merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 
+/*global FB*/
+
+function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+  console.log('statusChangeCallback');
+  console.log(response);                   // The current login status of the person.
+  if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+    testAPI();
+  } else {                                 // Not logged into your webpage or we are unable to tell.
+    document.getElementById('status').innerHTML = 'Please log ' +
+      'into this webpage.';
+  }
+}
+
+function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+  console.log('Welcome!  Fetching your information.... ');
+  FB.api('/me', function(response) {
+    console.log(response);
+    console.log('Successful login for: ' + response.name);
+    document.getElementById('status').innerHTML =
+      'Thanks for logging in, ' + response.name + '!';
+  });
+}
+
 function App() {
+  window.fbAsyncInit = function() {
+    FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
+      statusChangeCallback(response);        // Returns the login status.
+    });
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
-        <div class="fb-login-button" data-width="" data-size="medium" data-button-type="login_with" data-layout="rounded" data-auto-logout-link="true" data-use-continue-as="false" data-onlogin="test()"></div>
+        <div
+          className="fb-login-button"
+          data-width=""
+          data-size="medium"
+          data-button-type="login_with"
+          data-layout="rounded"
+          data-auto-logout-link="true"
+          data-use-continue-as="false"
+          data-onlogin="test()"
+          scope="public_profile,email">
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p id="status">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
