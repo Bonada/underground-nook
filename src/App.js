60a4f90af28f4a771340bb3d49c778fb199e93e8
sahith00 <sahithb00@gmail.com>
2021-10-27 16:46:26 -0400

    Moved FB login button to nav bar

--- src/App.js
+++ src/App.js
@@ -9,49 +9,9 @@ import Navigation from "./Components/Navigation";
 // import Orders from "./Pages/Orders";
 // import Settings from "./Pages/Settings";
 
-
-/*global FB*/
-
-function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
-  console.log('statusChangeCallback');
-  console.log(response);                   // The current login status of the person.
-  if (response.status === 'connected') {   // Logged into your webpage and Facebook.
-    testAPI();
-  } else {                                 // Not logged into your webpage or we are unable to tell.
-    document.getElementById('status').innerHTML = 'Please log ' +
-      'into this webpage.';
-  }
-}
-
-function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
-  console.log('Welcome!  Fetching your information.... ');
-  FB.api('/me', function(response) {
-    console.log(response);
-    console.log('Successful login for: ' + response.name);
-    document.getElementById('status').innerHTML =
-      'Thanks for logging in, ' + response.name + '!';
-  });
-}
-
-function App() {
-  window.fbAsyncInit = function() {
-    FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
-      statusChangeCallback(response);        // Returns the login status.
-    });
-  };
-  
+function App() {  
   return (
     <div className="App">
-      <div
-        className="fb-login-button"
-        data-width=""
-        data-size="medium"
-        data-button-type="login_with"
-        data-layout="rounded"
-        data-auto-logout-link="true"
-        data-use-continue-as="false"
-        scope="public_profile,email">
-      </div>
       <Router>
         <Navigation />
       </Router>
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -8,7 +8,36 @@ import Orders from "../Pages/Orders";
 import Settings from "../Pages/Settings";
 import './Navigation.css';
 
+/*global FB*/
+
+function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+  console.log('statusChangeCallback');
+  console.log(response);                   // The current login status of the person.
+  if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+    //testAPI();
+  } else {                                 // Not logged into your webpage or we are unable to tell.
+    document.getElementById('status').innerHTML = 'Please log ' +
+      'into this webpage.';
+  }
+}
+
+function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+  console.log('Welcome!  Fetching your information.... ');
+  FB.api('/me' {fields: 'name, email, picture'}, function(response) {
+    console.log(response);
+    console.log('Successful login for: ' + response.name);
+    document.getElementById('status').innerHTML =
+      'Thanks for logging in, ' + response.name + '!';
+  });
+}
+
 export default function Navigation() {
+  window.fbAsyncInit = function() {
+    FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
+      statusChangeCallback(response);        // Returns the login status.
+    });
+  };
+
   return (
     <Router>
       <div id="Post-Login_NavBar" className="Post-Login_NavBar">
@@ -34,6 +63,18 @@ export default function Navigation() {
         <i style={{margin: "0px"}} className="fa fa-user fa-lg" aria-hidden="true"></i>
         <div id="Links">
           <Link to="/Settings">Settings</Link>
+        </div>
+
+        <div
+          id="Links"
+          className="fb-login-button"
+          data-width=""
+          data-size="medium"
+          data-button-type="login_with"
+          data-layout="rounded"
+          data-auto-logout-link="true"
+          data-use-continue-as="false"
+          scope="public_profile,email">
         </div>
 
         {/* <div className="dropdown">
