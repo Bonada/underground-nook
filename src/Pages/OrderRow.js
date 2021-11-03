acb3e72310cd9728b4fd8d30104c9fac3a7abf1e
Wyvail <47842470+Wyvail@users.noreply.github.com>
2021-11-03 15:38:05 -0400

    Changed fonts and changed orders to table

--- src/Pages/OrderRow.js
+++ src/Pages/OrderRow.js
@@ -1,25 +1,11 @@
 function OrderRow() {
     return (
-        <div>
-            <div className="row">&nbsp;</div>
-            <div className="order row">
-                <div className="col-md-4">
-                    <h2 className="order-text">861 Michigan Ave <br /> Schenectady, NY 12309</h2>
-                </div>
-                <div className="col-md-4">
-                    <div className="row">&nbsp;</div>
-                    <h2 className="order-text">id1</h2>
-                </div>
-                <div className="col-md-2">
-                    <div className="row">&nbsp;</div>
-                    <h2 className="order-text">Paid</h2>
-                </div>
-                <div className="col-md-2">
-                    <div className="row">&nbsp;</div>
-                    <h2 className="order-text">Delivered</h2>
-                </div>
-            </div>
-        </div>
+        <tr>
+            <th scope="row">861 Michigan Ave <br />Schenectady, NY 12309</th>
+            <th className="align-middle">id1</th>
+            <th className="align-middle">Unpaid</th>
+            <th className="align-middle">Pending</th>
+        </tr>
     );
 }
 
--- src/Pages/Orders.js
+++ src/Pages/Orders.js
@@ -1,35 +1,31 @@
 import React from "react";
 import './Orders.css';
 import '../Components/Navigation.css';
-import OrderRow from "./OrderRow.js"
+import OrderRow from './OrderRow.js';
 
 function Orders() {
   return (
     <div className="container-fluid justify-content-center">
       <div className="row">
         <div className="offset-md-1 col-md-3">
-          <h1 className="catalog-header">Orders</h1>
+          <h1 className="orders-header">Orders</h1>
         </div>
       </div>
-      <div className="offset-md-1 col-md-10 orders-bg">
-        <div className="order row header">
-          <div className="col-md-4">
-            <h1>Address</h1>
-          </div>
-          <div className="col-md-4">
-            <h1>Order ID</h1>
-          </div>
-          <div className="col-md-2">
-            <h1>Paid?</h1>
-          </div>
-          <div className="col-md-2">
-            <h1>Status</h1>
-          </div>
-        </div>
-        <OrderRow />
-        <OrderRow />
-        <OrderRow />
-      </div>
+      <table className="orders table table-bg">
+        <thead>
+          <tr>
+            <th scope="col">Address</th>
+            <th scope="col">Order ID</th>
+            <th scope="col">Payment Status</th>
+            <th scope="col">Order Status</th>
+          </tr>
+        </thead>
+        <tbody class="table-body">
+          <OrderRow />
+          <OrderRow />
+          <OrderRow />
+        </tbody>
+      </table>
     </div>
   );
 }
