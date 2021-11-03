fa8b5b15ceb3a0e7328873780d83b00ea12c731f
Wyvail <47842470+Wyvail@users.noreply.github.com>
2021-11-03 16:33:24 -0400

    Update CatalogCard.js

--- src/Pages/CatalogCard.js
+++ src/Pages/CatalogCard.js
@@ -6,8 +6,8 @@ function CatalogCard() {
             <div className="catalog card" data-bs-toggle="modal" data-bs-target="#catalogModal">
                 <div className="row">
                     <img className="catalog-img card-img-top" src="https://i.etsystatic.com/25913068/r/il/1a565b/3138887356/il_1140xN.3138887356_4n0k.jpg" alt="PPP"></img>
-                    <h2 className="catalog-card-title card-title col-md-7 ">Pink Princess Philodendron</h2>
-                    <div className="catalog-price-col card-text col-md-4 ">
+                    <h2 className="catalog-card-title card-title col-md-8 ">Pink Princess Philodendron</h2>
+                    <div className="catalog-price-col card-text col-md-3 ">
                         <div className="row">&nbsp;</div>
                         <div className="row">
                             <p className="catalog-price col">$165+</p>
