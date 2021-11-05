0949439bc5d53b2c55b508f6d68dc3d6811e2aff
bonada <bonada@rpi.edu>
2021-11-04 23:50:46 -0400

    Populate cards with data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import PostLogin_Navigation from "./Components/PostLogin_Navigation";
 import PreLogin_Navigation from "./Components/PreLogin_Navigation";
 import Admin_Navigation from "./Components/Admin_Navigation";
+import Catalog from "./Pages/Catalog"
 // import About from "./Pages/About";
 // import Cart from "./Pages/Cart";
 // import Catalog from "./Pages/Catalog";
