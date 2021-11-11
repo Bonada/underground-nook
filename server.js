06db1fab40149a72bb84ca0c5d804e325f03f406
bonada <bonada@rpi.edu>
2021-11-11 00:33:26 -0500

    Revert "Update server.js"
    This reverts commit 1915b2d863bae42a7c97af72af9784bc564abe81.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 var bodyParser = require('body-parser');
 const { json } = require('body-parser');
 
-const app = express().use(express.static(path.join(__dirname, 'public')));
+const app = express();
 const cors = require('cors');
 app.use(cors());
 
