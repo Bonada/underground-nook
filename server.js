d117ce1a1a5e16c4dfc4333231081cec708b09b9
bonada <bonada@rpi.edu>
2021-11-11 00:33:13 -0500

    Revert "trying something horrible"
    This reverts commit 6d2fa768175f6c18df4326e3476c7f983cc8bb4b.

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 // import fetch from 'node-fetch';
-const path = require('path');
+
 var bodyParser = require('body-parser');
 const { json } = require('body-parser');
 
-const app = express();
+const app = express().use(express.static(path.join(__dirname, 'public')));
 const cors = require('cors');
 app.use(cors());
 
@@ -18,9 +18,6 @@ cloudinary.config({
 });
 
 const port = process.env.PORT || 3030;
-
-const routes = require('./routes/api');
-app.use('/api', routes);
 
 // function uploadimg() {
 //   cloudinary.uploader.upload("zbALL.jpg", 
@@ -49,243 +46,241 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 //   }
 // });
 
-app.get('/', function(req, res) {
-    console.log("HHIHIHIHIHIHI");
-    res.sendFile(path.resolve(__dirname + 'src/pages/Home.js'));
-});
+app.post('/add-user' , async (req, res) =>{
 
-// app.post('/add-user' , async (req, res) =>{
+  console.log("adding new user");
+  let username = req.body.username;
+  let userid = req.body.userid;
+  let email = req.body.email;
 
-//   console.log("adding new user");
-//   let username = req.body.username;
-//   let userid = req.body.userid;
-//   let email = req.body.email;
+  try {
+      await client.connect();
+      let db = client.db('main');
 
-//   try {
-//       await client.connect();
-//       let db = client.db('main');
+      let newUser = {userid: userid, username: username, email: email, addresses: []};
+      let users = db.collection('users');
 
-//       let newUser = {userid: userid, username: username, email: email, addresses: []};
-//       let users = db.collection('users');
+      let existinguser = users.findOne({userid:userid});
+      if (!existinguser) {
+        users.insertOne(newUser);
 
-//       let existinguser = users.findOne({userid:userid});
-//       if (!existinguser) {
-//         users.insertOne(newUser);
+        console.log("added new user");
 
-//         console.log("added new user");
+        res.json({
+            success: true,
+            err: 'Facebook user ' + username +  ' added to database'
+        });
+      }
+      else {
+        res.json({
+            success: false,
+            err: 'Facebook user ' + username +  'already exists'
+        });
+      }
+  } catch (e) {
+      res.status(400);
+      res.json({
+          success: false,
+          err: 'User ' + username + 'does not exist'
+      });
+  }
+})
 
-//         res.json({
-//             success: true,
-//             err: 'Facebook user ' + username +  ' added to database'
-//         });
-//       }
-//       else {
-//         res.json({
-//             success: false,
-//             err: 'Facebook user ' + username +  'already exists'
-//         });
-//       }
-//   } catch (e) {
-//       res.status(400);
-//       res.json({
-//           success: false,
-//           err: 'User ' + username + 'does not exist'
-//       });
-//   }
-// })
+app.get('/verify-user' , async (req, res) =>{
 
-// app.get('/verify-user' , async (req, res) =>{
-
-//     console.log("adding new user");
-//     let userid = req.body.userid;
+    console.log("adding new user");
+    let userid = req.body.userid;
   
-//     try {
-//         await client.connect();
-//         let db = client.db('main');
+    try {
+        await client.connect();
+        let db = client.db('main');
   
-//         let existinguser = db.collection('users').findOne({id:userid});
+        let existinguser = db.collection('users').findOne({id:userid});
   
-//         console.log("finding user");
-//         if(existinguser){
-//             res.json({
-//                 success: true,
-//                 err: 'Facebook user ' + userid +  ' added to database'
-//             });
-//         }
-//         res.json({
-//             success: false,
-//             err: 'Facebook user ' + userid +  'does not exist'
-//         });
+        console.log("finding user");
+        if(existinguser){
+            res.json({
+                success: true,
+                err: 'Facebook user ' + userid +  ' added to database'
+            });
+        }
+        res.json({
+            success: false,
+            err: 'Facebook user ' + userid +  'does not exist'
+        });
 
-//     } catch (e) {
-//         res.status(400);
-//         res.json({
-//             success: false,
-//             err: 'User ' + userid + 'does not exist'
-//         });
-//     }
-//   })
+    } catch (e) {
+        res.status(400);
+        res.json({
+            success: false,
+            err: 'User ' + userid + 'does not exist'
+        });
+    }
+  })
 
-// app.post('/add-plant' , async (req, res) =>{
+app.post('/add-plant' , async (req, res) =>{
 
-//   console.log("adding new plant");
-//   let id = uuidv4();
-//   let scientific_name = req.body.sname;
-//   let common_name = req.body.cname;
-//   let description = req.body.desc;
-//   let price = req.body.price;
+  console.log("adding new plant");
+  let id = uuidv4();
+  let scientific_name = req.body.sname;
+  let common_name = req.body.cname;
+  let description = req.body.desc;
+  let price = req.body.price;
 
-//   try {
-//       await client.connect();
-//       let db = client.db('main');
+  try {
+      await client.connect();
+      let db = client.db('main');
 
-//       let newPlant = {id: id, scientific_name: scientific_name, common_name: common_name, description: description};
+      let newPlant = {id: id, scientific_name: scientific_name, common_name: common_name, description: description};
 
-//       db.collection('plants').insertOne(newPlant);
+      db.collection('plants').insertOne(newPlant);
 
-//       console.log("added new plant");
+      console.log("added new plant");
 
-//       res.json({
-//           success: true,
-//           err: 'Plant ' + scientific_name +  'added to database'
-//       });
-//   } catch (e) {
-//       res.status(400);
-//       res.json({
-//           success: false,
-//           err: 'Error adding ' + scientific_name + 'to database'
-//       });
-//   }
-// })
+      res.json({
+          success: true,
+          err: 'Plant ' + scientific_name +  'added to database'
+      });
+  } catch (e) {
+      res.status(400);
+      res.json({
+          success: false,
+          err: 'Error adding ' + scientific_name + 'to database'
+      });
+  }
+})
 
-// app.post('/add-order' , async (req, res) =>{
+app.post('/add-order' , async (req, res) =>{
 
-//     console.log("adding new plant");
-//     let id = uuidv4();
-//     let username = req.body.username;
-//     let date = Date().toString();
-//     let time = Date().now();
-//     let address = req.body.address;
-//     let paymentmethod = req.body.paymentmethod;
-//     let paymentinfo = req.body.paymentinfo;
-//     let shippingcarrier = req.body.shippingcarrier;
-//     let plants = req.body.plants;
-//     let images = req.body.images;
-//     let scientific_name = req.body.sname;
+    console.log("adding new plant");
+    let id = uuidv4();
+    let username = req.body.username;
+    let date = Date().toString();
+    let time = Date().now();
+    let address = req.body.address;
+    let paymentmethod = req.body.paymentmethod;
+    let paymentinfo = req.body.paymentinfo;
+    let shippingcarrier = req.body.shippingcarrier;
+    let plants = req.body.plants;
+    let images = req.body.images;
+    let scientific_name = req.body.sname;
   
-//     try {
-//         await client.connect();
-//         let db = client.db('main');
+    try {
+        await client.connect();
+        let db = client.db('main');
 
-//         let newOrder = {id: id, username: username, plants: plants, date: date, time: time, address:address, paymentmethod: paymentmethod, paymentinfo: paymentinfo, shippingcarrier: shippingcarrier};
+        let newOrder = {id: id, username: username, plants: plants, date: date, time: time, address:address, paymentmethod: paymentmethod, paymentinfo: paymentinfo, shippingcarrier: shippingcarrier};
   
-//         db.collection('orders').insertOne(newOrder);
+        db.collection('orders').insertOne(newOrder);
   
-//         console.log("added new plant");
+        console.log("added new plant");
   
-//         res.json({
-//             success: true,
-//             err: 'Plant ' + scientific_name +  'added to database'
-//         });
-//     } catch (e) {
-//         res.status(400);
-//         res.json({
-//             success: false,
-//             err: 'Error adding ' + scientific_name + 'to database'
-//         });
-//     }
-//   })
+        res.json({
+            success: true,
+            err: 'Plant ' + scientific_name +  'added to database'
+        });
+    } catch (e) {
+        res.status(400);
+        res.json({
+            success: false,
+            err: 'Error adding ' + scientific_name + 'to database'
+        });
+    }
+  })
 
-// app.get('/get-plants' , async (req, res) =>{
-//   try {
+app.get('/get-plants' , async (req, res) =>{
+  try {
 
-//       console.log("connecting to db to get plants");
+      console.log("connecting to db to get plants");
 
-//       await client.connect();
-//       let db = client.db('main');
-//       let collection = db.collection('plants');
-//       let document = await collection.find();
-//       let items = await document.toArray();
+      await client.connect();
+      let db = client.db('main');
+      let collection = db.collection('plants');
+      let document = await collection.find();
+      let items = await document.toArray();
 
-//       console.log(items);
-//       res.send(items);
-//   }catch (e) {
-//       res.status(400);
-//       res.json({
-//           success: false,
-//           err: 'Cannot get the plant data'
-//       });
-//   }
-// })
+      console.log(items);
+      res.send(items);
+  }catch (e) {
+      res.status(400);
+      res.json({
+          success: false,
+          err: 'Cannot get the plant data'
+      });
+  }
+})
 
-// app.get('/get-orders' , async (req, res) =>{
-//     try {
+app.get('/get-orders' , async (req, res) =>{
+    try {
   
-//         console.log("connecting to db to get plants");
+        console.log("connecting to db to get plants");
   
-//         await client.connect();
-//         let db = client.db('main');
-//         let collection = db.collection('orders');
-//         let document = await collection.find();
-//         let items = await document.toArray();
+        await client.connect();
+        let db = client.db('main');
+        let collection = db.collection('orders');
+        let document = await collection.find();
+        let items = await document.toArray();
   
-//         console.log(items);
-//         res.send(items);
-//     }catch (e) {
-//         res.status(400);
-//         res.json({
-//             success: false,
-//             err: 'Cannot get the plant data'
-//         });
-//     }
-//   })
+        console.log(items);
+        res.send(items);
+    }catch (e) {
+        res.status(400);
+        res.json({
+            success: false,
+            err: 'Cannot get the plant data'
+        });
+    }
+  })
 
-//   app.get('/get-user-orders' , async (req, res) =>{
-//     try {
+  app.get('/get-user-orders' , async (req, res) =>{
+    try {
   
-//         let userid = req.body.userid;
-//         console.log("connecting to db to get user");
+        let userid = req.body.userid;
+        console.log("connecting to db to get user");
   
-//         await client.connect();
-//         let db = client.db('main');
-//         let collection = db.collection('users');
-//         let document = await collection.findOne({id: userid}, {orders: 1, userid: 0, addresses: 0, username: 0, email: 0});
+        await client.connect();
+        let db = client.db('main');
+        let collection = db.collection('users');
+        let document = await collection.findOne({id: userid}, {orders: 1, userid: 0, addresses: 0, username: 0, email: 0});
   
-//         console.log(document);
-//         res.send(document);
-//     }catch (e) {
-//         res.status(400);
-//         res.json({
-//             success: false,
-//             err: 'Cannot get the plant data'
-//         });
-//     }
-//   })
+        console.log(document);
+        res.send(document);
+    }catch (e) {
+        res.status(400);
+        res.json({
+            success: false,
+            err: 'Cannot get the plant data'
+        });
+    }
+  })
 
-// app.get('/get-user' , async (req, res) =>{
-//   try {
+app.get('/get-user' , async (req, res) =>{
+  try {
 
-//       let userid = req.body.userid;
-//       console.log("connecting to db to get user");
+      let userid = req.body.userid;
+      console.log("connecting to db to get user");
 
-//       await client.connect();
-//       let db = client.db('main');
-//       let collection = db.collection('user');
-//       let document = await collection.findOne({id: userid});
-//       let items = await document.toArray();
+      await client.connect();
+      let db = client.db('main');
+      let collection = db.collection('user');
+      let document = await collection.findOne({id: userid});
+      let items = await document.toArray();
 
-//       console.log(items);
-//       res.send(items);
-//   }catch (e) {
-//       res.status(400);
-//       res.json({
-//           success: false,
-//           err: 'Cannot get the plant data'
-//       });
-//   }
-// })
+      console.log(items);
+      res.send(items);
+  }catch (e) {
+      res.status(400);
+      res.json({
+          success: false,
+          err: 'Cannot get the plant data'
+      });
+  }
+})
+
+// app.use(express.static(__dirname + '/'));
+
 app.listen(port, () => {
-    console.log(`Listening on *: ${port}`)
+    console.log('Listening on *: 3030')
 });
 
 // Create an HTTP service.
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -39,7 +39,7 @@ export default function Navigation(props) {
       console.log(response);
 
       // If user doesn't exist in database, add their information and redirect to registration
-      fetch('/api/add-user', {
+      fetch('/add-user', {
         method: 'POST',
         mode: 'cors',
         headers: {
