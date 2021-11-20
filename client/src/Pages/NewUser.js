8f24b1206ff3e7da6d6c0fe895f1bf14fe910980
Helena O'Sullivan <osullh@rpi.edu>
2021-11-20 08:38:42 -0500

    New User Page Update

--- client/src/Pages/NewUser.js
+++ client/src/Pages/NewUser.js
@@ -4,33 +4,36 @@ import '../Components/Navigation.css';
 
 function NewUser() {
   return (
-    <div id="HomePage">
-      <h1 id="Home_Title">Welcome to the Underground Nook</h1>
-      <p>Hello new user! Before you continue on, please provide some additional information about yourself in order to enhance your experience.</p>
-
-      <form onSubmit="create-settings">
+    <div id="NewUser">
+      <div id="Welcome_Message">
+        <h1 id="User_Page_Title">Welcome to the Underground Nook</h1>
+        <p id="User_Page_Description">Hello new user! Before you continue on, please provide some additional information about yourself in order to enhance your experience.</p>
+      </div>
+      <form id="Settings_Info" onSubmit="create_user-settings">
         <div className="row">
-          <label htmlFor="fname" className="form-label heading2">First Name</label>
-          <input className="input-box" type="text" defaultValue="Minying" id="fname" />
+          <label htmlFor="firstname" id="NewUser-label" className="form-label heading">First Name</label>
+          <input className="input-box" type="text" defaultValue="John" id="firstname" />
         </div>
 
         <div className="row">
-          <label htmlFor="lname" className="form-label heading2">Last Name</label>
-          <input className="input-box" type="text" defaultValue="Cao" id="lname" />
+          <label htmlFor="lastname" id="NewUser-label" className="form-label heading">Last Name</label>
+          <input className="input-box" type="text" defaultValue="Doe" id="lastname" />
         </div>
 
         <div className="row">
-          <label htmlFor="email" className="form-label heading2">Email</label>
-          <input className="input-box" type="email" defaultValue="minyingcao12@gmail.com" id="email" />
+          <label htmlFor="email" id="NewUser-label" className="form-label heading">Email</label>
+          <input className="input-box" type="email" defaultValue="abc123@gmail.com" id="email" />
         </div>
 
         <div className="row">
-          <label htmlFor="phonenumber" className="form-label heading2">Phone Number</label>
+          <label htmlFor="phonenumber" id="NewUser-label" className="form-label heading">Phone Number</label>
           <input className="input-box" type="text" defaultValue="1234567890" id="phonenumber" />
         </div>
 
-        <div className="row flex-box-save-button">
-          <button className="btn save-button" type="button">Submit</button>
+        <div className="row flex-box-save-button" id="NewUserButton">
+          <button className="btn_save-button" type="button">
+            <h1 id="NewUser_ButtonText">Submit</h1>
+          </button>
         </div>
       </form>
     </div>
